d1e0203caadefeef94d92e1990d2e16537865034
Eugene Luo <eyluo@andrew.cmu.edu>
2019-10-16 11:05:38 -0400

    changed name, removed excess code

--- soundOutput.py
+++ soundOutput.py
@@ -7,15 +7,7 @@ import urllib
 import time
 import math
 import sys
-
 import time
-
-LANG_CODE = 'en-US'  # Language to use
-
-GOOGLE_SPEECH_URL = 'https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&pfilter=2&lang=%s&maxresults=6' % (LANG_CODE)
-
-FLAC_CONV = 'flac -f'  # We need a WAV to FLAC converter. flac is available
-                       # on Linux
 
 # Microphone stream config.
 CHUNK = 128  # CHUNKS of bytes to read each time from mic
@@ -28,35 +20,6 @@ THRESHOLD = 1000  # The threshold intensity that defines silence
 SILENCE_LIMIT = 1  # Silence limit in seconds. The max ammount of seconds where
                    # only silence is recorded. When this time passes the
                    # recording finishes and the file is delivered.
-
-PREV_AUDIO = 0.5  # Previous audio (in seconds) to prepend. When noise
-                  # is detected, how much of previously recorded audio is
-                  # prepended. This helps to prevent chopping the beggining
-                  # of the phrase.
-
-
-def audio_int(num_samples=50):
-    """ Gets average audio intensity of your mic sound. You can use it to get
-        average intensities while you're talking and/or silent. The average
-        is the avg of the 20% largest intensities recorded.
-    """
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    values = [math.sqrt(abs(audioop.avg(stream.read(CHUNK), 4))) 
-              for x in range(num_samples)] 
-    values = sorted(values, reverse=True)
-    r = sum(values[:int(num_samples * 0.2)]) / int(num_samples * 0.2)
-    stream.close()
-    p.terminate()
-    return r
-
 
 def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
     """
@@ -135,24 +98,5 @@ def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
 
     return response
 
-
-def save_speech(data, p):
-    """ Saves mic data to temporary WAV file. Returns filename of saved 
-        file """
-
-    filename = 'output_'+str(int(time.time()))
-    # writes data to WAV file
-    data = ''.join(data)
-    wf = wave.open(filename + '.wav', 'wb')
-    wf.setnchannels(1)
-    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-    wf.setframerate(16000)  # TODO make this value a function parameter?
-    wf.writeframes(data)
-    wf.close()
-    return filename + '.wav'
-
-
 if(__name__ == '__main__'):
     listen_for_speech()  # listen to mic.
-    #print stt_google_wav('hello.flac')  # translate audio file
-    #audio_int()  # To measure your mic levels
