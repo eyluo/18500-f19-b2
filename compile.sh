424e516517dc766a12e54c57fcb291a82a1a016d
Spencer Yu <syu04012012@gmail.com>
2019-10-19 16:38:02 -0400

    update speechrecognition proof of concept

---
+++ compile.sh
@@ -0,0 +1,3 @@
+source venv/bin/activate
+python -m compileall .
+echo compiled to __pycache__
--- sr.py
+++ sr.py
@@ -15,31 +15,36 @@ if __name__ == "__main__":
 
     # obtain audio from the microphone
     r = sr.Recognizer()
-    r.phrase_time_limit = .2 # secs
+    r.phrase_time_limit = 0 # secs
 
-    with sr.Microphone() as source:
-
-        if len(sys.argv) == 2:
-            r.energy_threshold = int(sys.argv[1])
-        else:
-            r.adjust_for_ambient_noise(source)
-
-        print("Say something!")
-        audio = r.listen(source)
-
-        t0 = time.time()
-
-    # recognize speech using Sphinx
     try:
-        recognized = r.recognize_sphinx(audio)
-        rec = True
-    except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-    except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
-    t1 = time.time()
+        while True:
+            with sr.Microphone() as source:
 
-    if rec:
-        print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+                if len(sys.argv) == 2:
+                    r.energy_threshold = int(sys.argv[1])
+                else:
+                    r.adjust_for_ambient_noise(source)
 
-    print("Audio Processing Time: {}".format(t1 - t0))
+                print("Say something!")
+                audio = r.listen(source)
+                print("Processing audio")
+                t0 = time.time()
+
+            # recognize speech using Sphinx
+            try:
+                recognized = r.recognize_sphinx(audio)
+                rec = True
+            except sr.UnknownValueError:
+                print("Sphinx could not understand audio")
+            except sr.RequestError as e:
+                print("Sphinx error; {0}".format(e))
+            t1 = time.time()
+
+            if rec:
+                print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+
+            print("Audio Processing Time: {}".format(t1 - t0))
+
+    except KeyboardInterrupt:
+        pass
