26da9ddeec0cdeae34ff5ed4ba5b6d1bd9988332
Spencer Yu <syu04012012@gmail.com>
2019-11-21 20:49:02 -0500

    WIP fastdtw

--- demo-dtw/soundOutput.py
+++ demo-dtw/soundOutput.py
@@ -11,9 +11,11 @@ import time
 import numpy as np
 import librosa
 from fastdtw import fastdtw
+import pdb
 
 # Microphone stream config.
-CHUNK = 128  # CHUNKS of bytes to read each time from mic
+OUTPUT_CHUNK_SIZE = 128 # CHUNK for jamming sound
+MIC_CHUNK_SIZE = 1024 # CHUNKS of bytes to read each time from mic
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
@@ -26,7 +28,7 @@ SAMPLES = "hey" # Folder to read reference data for error comparison from
 
 def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
     '''
-    Listens to Microphone, extracts audio sample of size CHUNK. If the audio amplitude
+    Listens to Microphone, extracts audio sample of size OUTPUT_CHUNK_SIZE. If the audio amplitude
     is greater than THRESHOLD, then processes audio sample into MFCC data.
     num_phrases controls how many phrases to process before finishing the listening process (-1 for infinite). 
 
@@ -41,10 +43,10 @@ def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
                     channels=CHANNELS,
                     rate=RATE,
                     input=True,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=OUTPUT_CHUNK_SIZE)
 
     cur_data = ''  # current chunk of audio data
-    rel = RATE/CHUNK
+    rel = RATE/OUTPUT_CHUNK_SIZE
     started = False
     n = num_phrases
     response = []
@@ -59,11 +61,11 @@ def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
         THRESHOLD = int(sys.argv[2])
     
     data = []
-    temp = wf.readframes(CHUNK)
+    temp = wf.readframes(OUTPUT_CHUNK_SIZE)
 
     while temp:
         data.append(temp)
-        temp = wf.readframes(CHUNK)
+        temp = wf.readframes(OUTPUT_CHUNK_SIZE)
 
     data = bytes().join(data)
     
@@ -82,7 +84,7 @@ def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
         fp = "{}/{}".format(SAMPLES,fp)
         print("File: {}".format(fp))
         t1 = time.time()
-        audio_data = librosa.core.audio.load(fp)
+        audio_data = librosa.core.audio.load(fp, sr=16000) # TODO: figure how to switch to hamming window
         t2 = time.time()
         ref_data.append(audio_data[0])
         print("Loading data:", t2 - t1)
@@ -94,26 +96,25 @@ def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
 
     lowest_dtw_dist = float('inf')
     while (num_phrases == -1 or n > 0):
-        cur_data = streamIn.read(CHUNK, exception_on_overflow=False)
+        cur_data = streamIn.read(MIC_CHUNK_SIZE, exception_on_overflow=False)
         t0 = time.time()
         np_data = np.array(list(cur_data), dtype=np.float32)
 
         if (started is True):
             streamIn.stop_stream()
 
-            clip_mfcc = librosa.feature.mfcc(np_data)
-            for dt in ref_data[1:]:
-                # do mfcc before DTW
-                ref_mfcc = librosa.feature.mfcc(dt)
-                ref_resized = np.resize(ref_mfcc[:, 0], (20, 1))
-                distance, path = fastdtw(ref_resized, clip_mfcc)
-                # print("ref_mfcc {} {}".format(type(ref_mfcc), ref_mfcc.shape))
-                # print("clip_mfcc {} {}".format(type(clip_mfcc), clip_mfcc.shape))
-                # print("ref_mfcc_resized {} {}".format(type(ref_resized), ref_resized.shape))
-                # print('DISTANCE {}'.format(distance))
+            clip_mfcc = librosa.feature.mfcc(np_data, sr=16000, n_mfcc=13) # Return shape: [n_mfcc, t]
+            clip_resized = clip_mfcc[1:, :] # Discard intensity vector. Shape: [n_mfcc - 1, t]
+            # for dt in ref_data[1:]:
+            dt = ref_data[0] # 0th data sample for testing purposes
+            # do mfcc before DTW
+            ref_mfcc = librosa.feature.mfcc(dt, sr=16000, n_mfcc=13) # Return shape: [n_mfcc, t]
+            ref_resized = ref_mfcc[1:, :] # Discard intensity vector. Shape: [n_mfcc - 1, t]
+            distance, path = fastdtw(np.transpose(ref_resized), np.transpose(clip_resized))
+            pdb.set_trace()
 
-                if (distance < lowest_dtw_dist):
-                    lowest_dtw_dist = distance
+            if (distance < lowest_dtw_dist):
+                lowest_dtw_dist = distance
             
             print("lowest dtw dist {}, threshold {}".format(lowest_dtw_dist, THRESHOLD_MSE))
             if (lowest_dtw_dist < THRESHOLD_MSE):
