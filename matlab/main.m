b1b5fd522a7a80823c54c13bfb1f4080dba4d2a3
Spencer Yu <syu04012012@gmail.com>
2019-11-25 12:30:15 -0500

    concat sound files works

--- matlab/main.m
+++ matlab/main.m
@@ -3,15 +3,15 @@ close all; clear; clc
 % load microphone info
 % 16kHz sample rate for 10ms of audio --> 10e-3 * 16kHz = 160 samples
 SAMPLE_RATE = 16e3; % 16 kHz
-FRAME_LENGTH = 10e-3; 
+FRAME_LENGTH = 30e-3; 
 NUM_SAMPLES = SAMPLE_RATE * FRAME_LENGTH;
 
 % load response sound file
 [ref_sound, fs_ref_sound] = audioread("dsp1.m4a");
+ref_resampled = resample(ref_sound, SAMPLE_RATE, fs_ref_sound);
 % [ref_sound, fs_ref_sound] = audioread("spencer_hey.wav");
-[ref_coeffs,ref_delta,ref_deltaDelta,ref_loc] = mfcc(ref_sound, fs_ref_sound, "LogEnergy","Ignore");
-% sound(ref_sound, fs_ref_sound, 8);
-% imagesc(ref_coeffs')
+[ref_coeffs,ref_delta,ref_deltaDelta,ref_loc] = mfcc(ref_resampled, SAMPLE_RATE, "LogEnergy","Ignore");
+% sound(ref_resampled, SAMPLE_RATE, 8);
 
 dct_row = flip(idct(ref_coeffs'), 1);
 
@@ -26,7 +26,7 @@ imagesc(dct_row);
 % nsc = floor(Nx/4.5);
 % nov = floor(nsc*.9);
 % nff = max(256,2^nextpow2(nsc));
-% 
+
 % subplot(2,1,1)
 % spectrogram(ref_sound,hamming(nsc),nov,nff, 'yaxis');
 % title('spectrogram() on the reference sound');
@@ -34,19 +34,32 @@ imagesc(dct_row);
 % imagesc(dct2(ref_coeffs));
 % title('dct2() on the MFCC coefficients');
 
-% 
-% % Initialize the audio IO stream
-% mic_in = audioDeviceReader(SAMPLE_RATE, NUM_SAMPLES);
-% 
+% Initialize the audio IO stream
+mic_in = audioDeviceReader(SAMPLE_RATE, NUM_SAMPLES);
+
+NUM_LOOP = 50;
+audio_data = zeros(NUM_LOOP, NUM_SAMPLES);
+for i=1:NUM_LOOP
+    temp = mic_in();
+    audio_data(i, :) = temp;
+end
+
+a = audio_data';
+a = a(:);
+
+sound(a, SAMPLE_RATE, 8);
+
 % mic_in();
 % mfcc_vals = zeros(1,13);
 % % loop on audio samples, begin logging values when volume threshold
 % % surpassed
+% NUM_LOOPS = 50;
+% audio_data = zeros(NUM_LOOPS,480);
 % 
-% for i = 1:19
-%     close all;
+% for i = 1:NUM_LOOPS
 %     tic;
 %     audioFromDevice = mic_in();
+%     audio_data(i, :) = audioFromDevice;
 % %     win = hann(NUM_SAMPLES,"periodic");
 % %     S = stft(audioFromDevice,"Window",win,"OverlapLength",512,"Centered",false);
 %     coeffs = mfcc(audioFromDevice, SAMPLE_RATE, "LogEnergy","Ignore");
@@ -70,3 +83,4 @@ imagesc(dct_row);
 % end
 % 
 % release(mic_in);
+% sound(audio_data(:), SAMPLE_RATE, 8);
