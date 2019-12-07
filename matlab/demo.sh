1fb62db955de47ce6a2c33ff40ab8388e5c09425
cyrusd98 <cyrus23daruwala@gmail.com>
2019-12-07 16:55:34 -0500

    demo for matlab

---
+++ matlab/demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# demo script to synchronize before executing python script
+
+TIME_SERVER=time.apple.com
+
+#if [ "$#" -ne 1 ]; then
+#  echo "Usage: $0 <threshold>" >&2
+#  exit 1
+#fi
+
+
+sudo sntp -sS $TIME_SERVER
+/Applications/MATLAB_R2018a.app/bin/matlab -nodisplay -nodesktop -r "run dtw2.m"
